daf7a56a4e5da82ad0e1f6cb07e586ca44753fc3
Yarik <yaroslav.gavrilov.1200@mail.ru>
2024-04-28 21:36:59 +0300

    outprint

--- attestation.cs
+++ attestation.cs
@@ -4,9 +4,11 @@ string[] result = new string[input.Length];
 for (int i = 0; i < input.Length; i++)
 {
     if (input[i].Length <= maxLength)
-
     {
         result[i] = input[i];
     }
-
+}
+foreach (string resultItem in result)
+{
+    Console.WriteLine(resultItem);
 }
