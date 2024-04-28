8863be548fe65dcce5fbf2d66ce852df9bc06dfe
Yarik <yaroslav.gavrilov.1200@mail.ru>
2024-04-28 21:19:01 +0300

    for

--- attestation.cs
+++ attestation.cs
@@ -1,2 +1,12 @@
 string[] input = {'yard', 'hello', 'dom', 'abc', '38'};
 int max_length = 3;
+string[] result = new string[input.Length];
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= maxLength)
+
+    {
+        result[i] = input[i];
+    }
+
+}
